6ebbdf855d51ee9ee8ca431d1d57b7e8bfd74f55
horologe <tokushigeeita@gmail.com>
2024-01-16 21:24:45 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello world</Button>;
+    </div>
+  );
+}
+
+export default App;
